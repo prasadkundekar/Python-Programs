068e5b016fa069ee71675977709261cbf9bcfe1a
Prasad Kundekar <prasadkundekar292@gmail.com>
2025-09-06 10:35:40 +0530

    Swap Two Numbers Program

---
+++ SwapTwoNumbers.py
@@ -0,0 +1,9 @@
+x = input("Enter Value for X: ")
+y = input("Enter Value for Y: ")
+
+temp = x
+x = y
+y = temp
+
+print("The Value of X after swapping: {}".format(x))
+print("The Value of Y after swapping: {}".format(y))
